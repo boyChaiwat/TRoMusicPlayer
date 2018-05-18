41d280bd2b9d42585c155d1f7b99ef8bbfd0aac2
Unknown <boychakuzaa@gmail.com>
2018-05-18 11:12:03 +0700

    Fix Layout

--- app/src/main/java/com/chaiwat/boy/tromusicplayer/app/PlayerActivity.kt
+++ app/src/main/java/com/chaiwat/boy/tromusicplayer/app/PlayerActivity.kt
@@ -1,12 +1,17 @@
 package com.chaiwat.boy.tromusicplayer.app
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class PlayerActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
     }
+
+
 }
